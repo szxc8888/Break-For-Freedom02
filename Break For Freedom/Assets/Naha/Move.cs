84a791d8aaf9b705cfd21d42a53208742db0117e
KBC11606048\kbc11606048 <syouta-n-16d@stu.kbc.ac.jp>
2018-01-11 14:11:32 +0900

    試作品

--- Break For Freedom/Assets/Naha/Move.cs
+++ Break For Freedom/Assets/Naha/Move.cs
@@ -4,14 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class Move : MonoBehaviour
 {
-
-    GameObject player, wear1, wear2, wear3, key1, Door1,Door12;
-
+   
+    GameObject player, wear1, wear2, wear3, key1, Door1, Door12;
+    public float speed = 5;
+    private Animator animator;
+    
 
     public Sprite moguraImage;
     public Sprite moguraImage2;
     public Sprite moguraImage3;
-
 
 
     void Start()
@@ -23,69 +24,133 @@ public class Move : MonoBehaviour
 
         this.Door1 = GameObject.Find("Door1");
 
- 
+        animator = GetComponent<Animator>();
+
+
     }
 
-    // Update is called once per frame
+
+
+
     void Update()
     {
+       
+        
+        // 右・左
+         float x = Input.GetAxisRaw("Horizontal");
+         if (Input.GetKey(KeyCode.RightArrow))
+         {
+             animator.SetFloat("Horizontal", 1f);
+             animator.SetFloat("Vertical", 0f);
+             transform.Translate(Time.deltaTime, 0, 0);
+         }
+         if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             animator.SetFloat("Horizontal", -1f);
+             animator.SetFloat("Vertical", 0f);
+             transform.Translate(-Time.deltaTime, 0, 0);
+         }
+        // 上・下
+         float y = Input.GetAxisRaw("Vertical");
+         if (Input.GetKey(KeyCode.UpArrow))
+         {
+             animator.SetFloat("Horizontal", 0f);
+             animator.SetFloat("Vertical", 1f);
+              transform.Translate(0, Time.deltaTime, 0);
+          }
+          if (Input.GetKey(KeyCode.DownArrow))
+          {
+              animator.SetFloat("Horizontal", 0f);
+              animator.SetFloat("Vertical", -1f);
+              transform.Translate(0, -Time.deltaTime, 0);
+          }
+        // 移動する向きを求める
+          Vector2 direction = new Vector2(x, y).normalized;
+        
+           // 移動する向きとスピードを代入する
+           animator.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
+
+
 
         // (↑)上に移動
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0, 0.1f, 0);
-        }
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    transform.Translate(0, 0.1f, 0);
+        //}
         // (↓)下に移動
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0, -0.1f, 0);
-        }
-        // (←)左に移動
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-0.1f, 0, 0);
-        }
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    transform.Translate(0, -0.1f, 0);
+        //}
+        //  (←)左に移動
+       // if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(-0.1f, 0, 0);
+        //}
         // (→)右に移動
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(0.1f, 0, 0);
-        }
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Translate(0.1f, 0, 0);
+        //}
 
-    }
+    //}
+
     // 服に触れたら着替える
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Wear1に触れたら着替える
-        if (other.gameObject.tag == "wear1")
-        {
 
-            this.tag = "wear1";                // タグをwear1に変更する
+        //Wear1に触れたら着替える
+        if (other.gameObject.tag == "wear3")
+        {
+            animator.Play("K Down");
+            // animator.Play("K Up");
+            //animator.SetBool("wear11", true);
+            //   animator.SetBool("wear22", false);
+            //   animator.SetBool("wear33", false);
+            this.tag = "wear3";
             SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();        // 画像を差し替える
             renderer.sprite = moguraImage;     // 画像変更枠追加
             // Destroy(wear1);      // Wear1削除
+
         }
 
 
-        // Wear2に触れたら着替える
-        if (other.gameObject.tag == "wear2")
-        {
-            this.tag = "wear2";                // タグをwear2に変更する
-            SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();        // 画像を差し替える
-            renderer.sprite = moguraImage2;    // 画像変更枠追加
-            //  Destroy(wear1);    // Wear1削除
-            //  Destroy(wear2);    // Wear2削除
-        }
+            // Wear2に触れたら着替える
+            if (other.gameObject.tag == "wear1")
+            {
 
-        // Wear3に触れたら着替える
-        if (other.gameObject.tag == "wear3")
-        {
-            this.tag = "wear3";                // タグをwear3の変更する
-            SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();        // 画像を差し替える
-            renderer.sprite = moguraImage3;    // 画像変更枠追加
-            //  Destroy(wear1);      // Wear1削除
-            //  Destroy(wear2);      // Wear2削除
-            //  Destroy(wear3);      // Wear3削除
+                animator.Play("S Down");
+           //   animator.SetBool("wear11", false);
+           //   animator.SetBool("wear22", true);
+           //   animator.SetBool("wear33", false);
+                this.tag = "wear1";                // タグをwear2に変更する
+                SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();        // 画像を差し替える
+                renderer.sprite = moguraImage2;    // 画像変更枠追加
+                //  Destroy(wear1);    // Wear1削除
+                //  Destroy(wear2);    // Wear2削除
+            }
 
+            // Wear3に触れたら着替える
+            if (other.gameObject.tag == "wear2")
+            {
+                animator.Play("EK Down");
+              //  animator.SetBool("wear11", false);
+              //  animator.SetBool("wear22", false);
+              //  animator.SetBool("wear33", true);
+                this.tag = "wear2";                // タグをwear3の変更する
+                SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();        // 画像を差し替える
+                renderer.sprite = moguraImage3;    // 画像変更枠追加
+                //  Destroy(wear1);      // Wear1削除
+                //  Destroy(wear2);      // Wear2削除
+                //  Destroy(wear3);      // Wear3削除
+
+            }
         }
     }
-}
+
+
+
+   
+
+  
