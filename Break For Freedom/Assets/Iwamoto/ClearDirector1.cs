00c803661ad71c8b7d5b6442292066d2acd9d1e9
KBC11606068\OS <naoyuki-i-16d@stu.kbc.ac.jp>
2018-01-31 13:04:21 +0900

    仕上げ

--- Break For Freedom/Assets/Iwamoto/ClearDirector1.cs
+++ Break For Freedom/Assets/Iwamoto/ClearDirector1.cs
@@ -2,28 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;     //UIを使用可能にする
+using UnityEngine.SceneManagement;
 
 public class ClearDirector1 : MonoBehaviour {
 
-    //private GameObject Clear; //点滅させたい文字
-    public float speed = 0.01f;  //透明化の速さ
-    float alfa;    //A値を操作するための変数
-    float red, green, blue;    //RGBを操作するための変数
 
 	// Use this for initialization
 	void Start () {
 
-        //Clear = GameObject.Find("/Canvas/Clear");
 
-        //Panelの色を取得
-        red = GetComponent<Text>().color.r;
-        green = GetComponent<Text>().color.g;
-        blue = GetComponent<Text>().color.b;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Text>().color = new Color(red, green, blue, alfa);
-        alfa += speed;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameTitle 2");
+        }
 	}
 }
