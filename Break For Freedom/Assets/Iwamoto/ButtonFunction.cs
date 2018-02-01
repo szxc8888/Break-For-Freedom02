9938256200fa7eef3dd5ace51b7c3bb29872aac9
KBC11606068\OS <naoyuki-i-16d@stu.kbc.ac.jp>
2018-02-01 11:51:38 +0900

    o

--- Break For Freedom/Assets/Iwamoto/ButtonFunction.cs
+++ Break For Freedom/Assets/Iwamoto/ButtonFunction.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ButtonFunction : MonoBehaviour
 {
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 00c803661ad71c8b7d5b6442292066d2acd9d1e9
     public void StringArgFunction(string s)
     {
         if (Input.GetKey(KeyCode.Space))
